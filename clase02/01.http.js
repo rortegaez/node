bb71abf56a966f766190951737d182e6c913ea9d
rortegaez <you@example.com>
2024-08-25 19:15:39 +0200

    "second push"

--- clase02/01.http.js
+++ clase02/01.http.js
@@ -3,10 +3,10 @@ const http = require('node:http')
 const desirePort = process.env.PORT ?? 1234
 
 const server = http.createServer((req, res) => {
-	console.log('request recived: ', req.url)
-	res.end('Hola mundo')
+  console.log('request recived: ', req.url)
+  res.end('Hola mundo')
 })
 
 server.listen(desirePort, () => {
-	console.log(`Server listening on port http://localhost:${desirePort}`)
+  console.log(`Server listening on port http://localhost:${desirePort}`)
 })
