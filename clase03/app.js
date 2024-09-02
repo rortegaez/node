a903fabd92fb9becb1db993d6aab26770dfb19d5
rortegaez <raulor83@gmail.com>
2024-09-02 21:23:07 +0200

    ¿end? clase03

--- clase03/app.js
+++ clase03/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const crypto = require('node:crypto')
 const movies = require('./movies.json')
+// const cors = require('cors')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
+// app.use(cors()) esta solución, lo que hace es poner asterisco apra que no haya ningún tipo de control.
 app.disable('x-powered-by')
 
 app.use(express.json())
@@ -12,8 +14,20 @@ app.get('/', (req, res) => {
 	res.json({ message: "hola mundo"})
 })
 
+const ACCEPTED_ORIGINS = [
+	'http://localhost:8080',
+	'http://localhost:1234',
+	'https://movies.com',
+	'https://midu.dev'
+]
+
 // recuperar todas las peliculas
 app.get('/movies', (req, res) => {
+	const origin = req.header('origin')
+	if(ACCEPTED_ORIGINS.includes(origin) || !origin) {
+		res.header('Access-Control-Allow-Origin', origin)
+	}
+
 	const { genre } = req.query
 	if(genre) {
 		const filteredMovies = movies.filter (
@@ -41,6 +55,19 @@ app.post('/movies', async (req, res) => {
 	res.status(201).json(newMovie)
 })
 
+app.delete('/movies/:id', (req, res) => {
+	const { id } = req.params
+	const movieIndex = movies.findeIndex(movie => movie.id === id)
+
+	if(movieIndex === -1){
+		return res.status(404).json({ message: 'Movie not found'})
+	}
+
+	movies.splices(movieIndex, 1)
+
+	return res.json({ message: 'Movie deleted'})
+})
+
 app.patch('/movies/:id', (req, res) => {
 	const result = validatePartialMovie(req.body )
 
@@ -63,6 +90,15 @@ app.patch('/movies/:id', (req, res) => {
 	movies[movieIndex] = updateMovie
 
 	return res.json(updateMovie)
+})
+
+app.options('/movies/:id', (req, res) => {
+	const origin = req.header('origin')
+	if(ACCEPTED_ORIGINS.includes(origin) || !origin) {
+		res.header('Access-Control-Allow-Origin', origin)
+		res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+	}
+	res.send()
 })
 
 
