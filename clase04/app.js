a9804c0bc272655a81ba70a5474f572018d471e6
rortegaez <raulor83@gmail.com>
2024-09-08 12:05:22 +0200

    pdte foro

--- clase04/app.js
+++ clase04/app.js
@@ -2,14 +2,7 @@ import express, { json } from 'express'
 import { corsMiddleware } from './middlewares/cors.js'
 import { moviesRouter } from './routes/movies.js'
 
-// como leer un json en ESModules
-// import fs from 'node:fs'
-// const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
-
-// const cors = require('cors')
-
 const app = express()
-// app.use(cors()) esta solución, lo que hace es poner asterisco apra que no haya ningún tipo de control.
 app.use(json())
 app.use(corsMiddleware())
 app.disable('x-powered-by')
@@ -18,7 +11,6 @@ app.get('/', (req, res) => {
   res.json({ message: 'hola mundo' })
 })
 
-// recuperar todas las peliculas
 app.use('/movies', moviesRouter)
 
 const PORT = process.env.PORT ?? 1234
